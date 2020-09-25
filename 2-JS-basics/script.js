464e8519030294b99883f636ed798d9267a53fc5
hugodmorais <hugodanielmorais@gmail.com>
2020-09-25 22:25:53 +0000

    Operators

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -33,3 +33,15 @@ console.log(typeof ageJohn); // number
 var x;
 console.log(typeof x); // undefined
 
+// Operator precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge); // true
+
+var ageJohn = now - yearJohn;
+
+
