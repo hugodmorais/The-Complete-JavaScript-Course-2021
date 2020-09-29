86066e7da2c704f2621c49a251b532d1f3f1a397
hugodmorais <hugodanielmorais@gmail.com>
2020-09-29 22:42:30 +0000

    Arrays

---
+++ 2-JS-basics/arrays.js
@@ -0,0 +1,9 @@
+/************************************************************
+* Arrays
+*/
+
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[0]);
+console.log(names.length);
