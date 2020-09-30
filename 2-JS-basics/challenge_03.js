c0e110a48299b4f51a64b38d880f44805380b645
hugodmorais <hugodanielmorais@gmail.com>
2020-09-30 22:08:51 +0000

    Challenge 03

---
+++ 2-JS-basics/challenge_03.js
@@ -0,0 +1,29 @@
+// Challenge 03
+
+var rest01, rest02, rest03, tips, final;
+
+tips = [];
+final = [];
+rest01 = 124;
+rest02 = 48;
+rest03 = 268;
+
+
+
+
+
+
+var tip_calculator = function(bill) {
+  console.log("teste" + bill);
+  switch(bill) {
+    case "teste":
+      return "bill * 0.2";
+  }
+}
+
+      
+console.log(tip_calculator("teste"))
+
+
+
+
