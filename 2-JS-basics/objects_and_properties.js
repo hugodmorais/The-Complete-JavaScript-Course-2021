d73306d7bb60930a07e11d9a9ae708b7693a3ffe
hugodmorais <hugodanielmorais@gmail.com>
2020-10-02 22:11:17 +0000

    objects functions

--- 2-JS-basics/objects_and_properties.js
+++ 2-JS-basics/objects_and_properties.js
@@ -19,3 +19,20 @@ jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 
 console.log(jane)
+
+/******************************************************/
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function(birthYear) {
+    return 2018 - this.birthYear;
+  }
+};
+
+console.log(john.calcAge(1990));
+
