49993130913e9a00de80629eacdb583d55600a71
hugodmorais <hugodanielmorais@gmail.com>
2020-10-04 21:52:16 +0000

    3 how JS works

---
+++ 3-how-JS-works/starter/script.js
@@ -0,0 +1,74 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+calculateAge(1999);
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
