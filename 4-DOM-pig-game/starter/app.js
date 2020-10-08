f0f85ab0d36e638aa1c68bc7495dd1f720c20a57
hugodmorais <hugodanielmorais@gmail.com>
2020-10-08 22:10:20 +0000

    winningScore

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -18,7 +18,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var six_repeat = false
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
-
+    
     dice_values.push(dice)
     if ((dice_values[dice_values.length - 1] + dice_values[dice_values.length - 2]) == 12)
       six_repeat = true 
@@ -53,8 +53,19 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    var input = document.querySelector('.final-score').value;
+    var winningScore;
+        
+    // Undefined, 0, null or "" are COERCED to false
+    // Anything else is COERCED to true
+    if(input) {
+        winningScore = input;
+    } else {
+        winningScore = 100;
+    }
+
     // Check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
