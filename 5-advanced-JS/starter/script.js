36e9fd20ad4f860b80f68a943e6cbc3786a79afe
hugodmorais <hugodanielmorais@gmail.com>
2020-10-11 21:51:22 +0000

    Object new and Object create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,32 +1,51 @@
 /////////////////////////////
 // Lecture: Function constructor
 
-var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job: 'teacher'
+// var john = {
+//     name: 'John',
+//     yearOfBirth: 1990,
+//     job: 'teacher'
+// };
+
+// var Person = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// }
+
+// Person.prototype.calculateAge  = function() {
+//   console.log(2016 - this.yearOfBirth);
+// };
+
+// Person.prototype.lastName = 'Smith';
+
+// var john = new Person('John', 1990, 'teacher');
+// var jane = new Person('Jane', 1969, 'designer');
+// var mark = new Person('Mark', 1948, 'retired');
+
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+
+// console.log(john.lastName);
+// console.log(jane.lastName);
+// console.log(mark.lastName);
+
+
+// Object.create
+var personProto = {
+  calculateAge: function() {
+    console.log(2016 - this.yearOfBirth);
+  }
 };
 
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-}
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1985;
+john.job = 'teacher';
 
-Person.prototype.calculateAge  = function() {
-  console.log(2016 - this.yearOfBirth);
-};
-
-Person.prototype.lastName = 'Smith';
-
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
-
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+var jane = Object.create(personProto, {
+  name: { value: 'Jane' },
+  yearOfBirth: { value: 1985 },
+  job: { value: 'designer' }
+});
