84dafe34a0d2f8b3eaf11ab548df43b91696912d
hugodmorais <hugodanielmorais@gmail.com>
2020-10-13 22:52:19 +0000

    Functions as arguments

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -85,3 +85,25 @@ change(age, obj);
 
 console.log(age); // 27
 console.log(obj.city); // San Francisco
+
+
+/////////////////////////////////////////////////////////////
+// Lecture: Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2020 - el;
+}
+
+var ages = arrayCalc(years, calculateAge);
+
+console.log(ages);
