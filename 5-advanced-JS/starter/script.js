1289b0b3e5d65eec088348a903d9ab6ba5f430b4
hugodmorais <hugodanielmorais@gmail.com>
2020-10-20 21:37:58 +0000

    Closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -170,4 +170,60 @@ interviewQuestion('teacher')('Mark');
 })(5);
 
 
-console.log(score); // score is not defined
+// console.log(score); // score is not defined
+
+
+/////////////////////////////////////////////////////////
+// Lecture: Closures
+
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+  return function(yearOfBirth) {
+    var age_hm = 2016 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+};
+
+var retirementUS = retirement(66);
+console.log(age_hm)
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990); // 39 years left until retirement.
+retirementUS(1990); // 40 years left until retirement.
+retirementIceland(1990); // 41 years left until retirement.
+
+
+/*------------------------------------------------------------*/
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+/* ------------------------->>>>> */
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+};
+
+interviewQuestion('teacher')('John'); // What subject do you teach, John?
