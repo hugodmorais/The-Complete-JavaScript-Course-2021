c9093b13acf0085987dd6ff8dbbcd5a73171d0d1
hugodmorais <hugodanielmorais@gmail.com>
2020-10-25 22:51:50 +0000

    Modules BudgetController, UIController, controller

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,30 @@
+/* budgetController */
+var budgetController = (function() {
+  var x = 23;
+
+  var add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b) {
+      return add(b);
+    }
+  }
+})();
+
+/* UIController */
+var UIController = (function() {
+
+})();
+
+/* controller */
+var controller = (function(budgetCtrl, UICtrl) {
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z);
+    }
+  }
+})(budgetController, UIController);
