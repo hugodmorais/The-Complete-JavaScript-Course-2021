eace4388cae1388cf267d8445f6663ae3eb9a480
hugodmorais <hugodanielmorais@gmail.com>
2020-10-27 14:04:05 +0000

    Input Object

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,25 +1,50 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
- 
 })();
 
 // UI CONTROLLER
 var UIController = (function() {
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  }
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var ctrlAddItem = function() {
+  var DOM = UICtrl.getDOMstrings();
 
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    var input = UICtrl.getInput()
+    console.log(input);
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
+
     if (event.keyCode === 13 || event.which === 13) {
-      console.log("13")
+      ctrlAddItem();
     }
+    
   });
 })(budgetController, UIController);
