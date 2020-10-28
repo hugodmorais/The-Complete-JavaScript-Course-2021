874d6ba06c08e9ab8158c908641cb05cc53aff43
hugodmorais <hugodanielmorais@gmail.com>
2020-10-28 23:56:19 +0000

    Let, var, const and type_of

--- Fundamentals/01-Fundamentals-Part-1/script.js
+++ Fundamentals/01-Fundamentals-Part-1/script.js
@@ -27,63 +27,27 @@ console.log(myFirstJob);
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 // console.log(typeof true);
-console.log(typeof javascriptIsFun);
+console.log(typeof javascriptIsFun); // boolean
 // console.log(typeof 23);
 // console.log(typeof 'Jonas');
 javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
+console.log(typeof javascriptIsFun); // string
 let year;
-console.log(year);
-console.log(typeof year);
+console.log(year); // undefined
+console.log(typeof year); // undefined
 year = 1991;
-console.log(typeof year);
-console.log(typeof null);
+console.log(typeof year); // number
+console.log(typeof null); // object
 
 ////////////////////////////////////
 // let, const and var
 let age = 30;
 age = 31;
 const birthYear = 1991;
-// birthYear = 1990;
-// const job;
+// birthYear = 1990; not work, cannot reset
+// const job; need initial value
 var job = 'programmer';
 job = 'teacher'
 lastName = 'Schmedtmann';
 console.log(lastName);
-////////////////////////////////////
-// Basic Operators
-// Math operators
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-console.log(firstName + ' ' + lastName);
-// Assignment operators
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--;
-x--;
-console.log(x);
-// Comparison operators
-console.log(ageJonas > ageSarah); // >, <, >=, <=
-console.log(ageSarah >= 18);
-const isFullAge = ageSarah >= 18;
-console.log(now - 1991 > now - 2018);
-////////////////////////////////////
-// Operator Precedence
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(now - 1991 > now - 2018);
-let x, y;
-x = y = 25 - 10 - 5; // x = y = 10, x = 10
-console.log(x, y);
-const averageAge = (ageJonas + ageSarah) / 2;
-console.log(ageJonas, ageSarah, averageAge);
 
