9269930af292c1a1439a60c3c808e361efe2bb2b
hugodmorais <hugodanielmorais@gmail.com>
2020-11-01 21:22:01 +0000

    Functions and arrow functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -27,3 +27,31 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 const num = Number('23');
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+// Function declaration
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+// Function expression
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+///////////////////////////////////////
+// Arrow functions
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas')); 
+console.log(yearsUntilRetirement(1980, 'Bob'));
