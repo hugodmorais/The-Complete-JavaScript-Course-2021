f1c902e5ab829741a27089e3327da96d496b850d
hugodmorais <hugodanielmorais@gmail.com>
2020-11-18 22:05:08 +0000

    Modal project

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -6,4 +6,16 @@ const btnCloseModel = document.querySelector('.close-modal');
 const btnsOpenModel = document.querySelectorAll('.show-modal');
 
 for(let i = 0; i < btnsOpenModel.length; i++)
-  console.log(btnsOpenModel[i]);
+  btnsOpenModel[i].addEventListener('click', function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+ const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+
+btnCloseModel.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
